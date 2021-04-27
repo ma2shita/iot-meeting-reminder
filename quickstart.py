35a1f198bfbc9aacf444e432cae31ab7d01d2aca
Kohei MATSUSHITA <ma2shita+git@ma2shita.jp>
2021-04-27 09:38:34 +0900

    Modify the license

--- quickstart.py
+++ quickstart.py
@@ -1,16 +1,9 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Original is https://github.com/googleworkspace/python-samples/commit/41f82a79bcd67ba4de329126a8fdcc4e3c0826ff
+# Change history is after https://github.com/ma2shita/iot-meeting-reminder/commit/69828d5b2b5c0ba88262d6af2bf31d26b343c22e
+# 
+# NOTE:
+#   The license of the original quickstart.py is Apache 2.0.
+#   This file has been modified to comply with the license of this entire repository.
 
 # [START calendar_quickstart]
 from __future__ import print_function
